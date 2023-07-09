7c6138df3849e12ded15f770fdbd16510ff9f6c7
Francisco Andre <55844517+Isco170@users.noreply.github.com>
2023-07-09 21:47:32 +0200

    Create Category done

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 import { CreateProductController } from './controller/createProductController';
+import { CreateCategoriesController } from './controller/createCategoriesController';
 
 const router = Router();
 
 const createProduct = new CreateProductController();
+const createCategory = new CreateCategoriesController();
 
 router.post("/product", createProduct.handle);
+router.post("/category", createCategory.handle);
 
 export { router };
